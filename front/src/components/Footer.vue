ebb29bb5b4cc0c54e6a23279bce61d67c39128bb
BestUO <31505068+BestUO@users.noreply.github.com>
2022-11-30 09:46:14 +0000

    备案链接

--- front/src/components/Footer.vue
+++ front/src/components/Footer.vue
@@ -3,7 +3,7 @@
   <div class="footer">
     <!-- <div>全栈修炼 ©2018 Created by BiaoChenXuYing</div> -->
 
-        苏ICP备2021034228号-1 苏ICP备2021034228号-2
+        <a href="https://beian.miit.gov.cn/" target="_blank">苏ICP备2021034228号-1 苏ICP备2021034228号-2</a>
 
   </div>
 </template>
